c98844423cd388332596baa72a1d8add50797dc2
Nqrbert Yagmurpv <nuri.handsome45@gmail.com>
2024-02-11 22:43:50 +0300

    added the ability to press buttons

--- app/src/main/java/com/example/necoru7fishermanbook/MainActivity.kt
+++ app/src/main/java/com/example/necoru7fishermanbook/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.necoru7fishermanbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val nav_view = findViewById<NavigationView>(R.id.nav_view)
+        nav_view.setNavigationItemSelectedListener(this)
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId)
+        {
+            R.id.id_fish -> Toast.makeText(this, "Fish selected", Toast.LENGTH_SHORT).show()
+            R.id.id_na -> Toast.makeText(this, "Nazhivki selected", Toast.LENGTH_SHORT).show()
+            R.id.id_sna -> Toast.makeText(this, "Snasti selected", Toast.LENGTH_SHORT).show()
+            R.id.id_history -> Toast.makeText(this, "History selected", Toast.LENGTH_SHORT).show()
+
+        }
+
+
+        return true
     }
 }
