11a30ae7550e9d2ac2221c768ec3df70eaacd7e7
PurulenT <nuri.handsome45@gmail.com>
2024-09-05 00:11:05 +0300

    Neco lesson 10

--- app/src/main/java/com/example/necoru7fishermanbook/ListItem.kt
+++ app/src/main/java/com/example/necoru7fishermanbook/ListItem.kt
@@ -1,4 +1,7 @@
 package com.example.necoru7fishermanbook
 
-class ListItem {
-}
+class ListItem (
+    var image_id:Int,
+    var title_text:String,
+    var content_text:String
+)
--- app/src/main/java/com/example/necoru7fishermanbook/MainActivity.kt
+++ app/src/main/java/com/example/necoru7fishermanbook/MainActivity.kt
@@ -1,25 +1,55 @@
 package com.example.necoru7fishermanbook
 
+import android.content.res.TypedArray
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    var adapter:MyAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val nav_view = findViewById<NavigationView>(R.id.nav_view)
         nav_view.setNavigationItemSelectedListener(this)
 
+        var list = ArrayList<ListItem>()
+
+        list.addAll(fillArrays(resources.getStringArray(R.array.fish),
+            resources.getStringArray(R.array.fish_content), getImageId(R.array.fish_image_array)))
+
+
+        var rcView = findViewById<RecyclerView>(R.id.rcView)
+        rcView.hasFixedSize()
+        rcView.layoutManager = LinearLayoutManager(this)
+
+        adapter = MyAdapter(list, this)
+        rcView.adapter = adapter
+
+
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId)
         {
-            R.id.id_fish -> Toast.makeText(this, "Fish selected", Toast.LENGTH_SHORT).show()
-            R.id.id_na -> Toast.makeText(this, "Nazhivki selected", Toast.LENGTH_SHORT).show()
+            R.id.id_fish ->
+            {
+                Toast.makeText(this, "Fish selected", Toast.LENGTH_SHORT).show()
+                adapter?.updateAdapter(fillArrays(resources.getStringArray(R.array.fish),
+                    resources.getStringArray(R.array.fish_content),
+                    getImageId(R.array.fish_image_array)))
+            }
+            R.id.id_na ->
+            {
+                Toast.makeText(this, "Nazhivki selected", Toast.LENGTH_SHORT).show()
+                adapter?.updateAdapter(fillArrays(resources.getStringArray(R.array.na_fish),
+                    resources.getStringArray(R.array.na_fish_content),
+                    getImageId(R.array.na_fish_image_array)))
+            }
             R.id.id_sna -> Toast.makeText(this, "Snasti selected", Toast.LENGTH_SHORT).show()
             R.id.id_history -> Toast.makeText(this, "History selected", Toast.LENGTH_SHORT).show()
 
@@ -28,4 +58,29 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         return true
     }
+
+    fun fillArrays(titleArray:Array<String>, contentArray:Array<String>, imageArray:IntArray):List<ListItem>
+    {
+        var listItemArray = ArrayList<ListItem>()
+        for(n in 0 .. titleArray.size - 1)
+        {
+            var listItem = ListItem(imageArray[n], titleArray[n], contentArray[n])
+            listItemArray.add(listItem)
+        }
+        return listItemArray
+    }
+
+    fun getImageId(imageArrayId:Int):IntArray
+    {
+        var tArray:TypedArray = resources.obtainTypedArray(imageArrayId)
+        val count = tArray.length()
+        val ids = IntArray(count)
+        for(i in ids.indices)
+        {
+            ids[i] = tArray.getResourceId(i, 0)
+        }
+        tArray.recycle()
+        return ids
+    }
+
 }
